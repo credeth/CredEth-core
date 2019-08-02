f38f1724ce7cf5e84170801142b6a6afa5d44dda
satyam <satyam@secureblocks.io>
2019-08-02 23:29:09 +0530

    initial commit

---
+++ truffle.js
@@ -0,0 +1,27 @@
+require('babel-register');
+require('babel-polyfill');
+
+module.exports = {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // Match any network id
+      gas: 7900000,
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.5.8",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }
+  },
+  mocha: {
+    enableTimeouts: false
+  }
+};
