70d31d0e53f2d411a64e2d8ff77f55189cf0f257
satyam <satyam@secureblocks.io>
2019-08-03 14:51:54 +0530

    minor fixes

--- contracts/credEth.sol
+++ contracts/credEth.sol
@@ -1,9 +1,8 @@
 pragma solidity ^0.5.7;
 
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-contract CredEth is Ownable {
+contract CredEth {
 
     using SafeMath for uint256;
     
